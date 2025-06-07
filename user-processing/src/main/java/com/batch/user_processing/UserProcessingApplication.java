67989bcb0880e48fad3e15ebfba89fa5de81ed31
ganeshkhatmode <ganeshkhatmode@gmail.com>
2025-06-08 01:08:36 +0530

    add project template

---
+++ user-processing/src/main/java/com/batch/user_processing/UserProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.batch.user_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProcessingApplication.class, args);
+	}
+
+}
